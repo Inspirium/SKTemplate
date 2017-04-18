e7a1e109429fa33b32e7492147f38e2232840a67
Marko Banusic <mbanusic@gmail.com>
2017-04-18 11:12:13 +0200

    Add values to form for existing users

--- src/views/user/edit.blade.php
+++ src/views/user/edit.blade.php
@@ -6,11 +6,11 @@
         {{ csrf_field() }}
         <div class="form-group">
             <label for="name">@lang("Name")</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="@lang("Enter Name")" required>
+            <input type="text" class="form-control" id="name" name="name" placeholder="@lang("Enter Name")" required value="{{ $user->name }}">
         </div>
         <div class="form-group">
             <label for="email">@lang("Email Address")</label>
-            <input type="email" class="form-control" id="email" name="email" placeholder="@lang("Enter Email")" required>
+            <input type="email" class="form-control" id="email" name="email" placeholder="@lang("Enter Email")" required value="{{ $user->email }}">
         </div>
         <div class="form-group">
             <label for="password">@lang("Password")</label>
