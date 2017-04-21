97b9bd1764f11889281e6fa05eb7089835bf6c94
StjepanD <sdrmic@gmail.com>
2017-04-21 09:46:11 +0200

    User role izmjene

--- src/views/user/role/user_roles.blade.php
+++ src/views/user/role/user_roles.blade.php
@@ -1,15 +1,32 @@
 @extends('sktemplate::layout')
 
 @section('content')
-<h1>@lang('User Roles')</h1>
-<form method="post" action="{{ url('user/roles/' . $user->id) }}">
-    {{ csrf_field() }}
-    @foreach($roles as $role)
-        <fieldset class="form-group">
-            <input type="checkbox" id="checkbox{{ $role->id }}" name="roles[]" value="{{ $role->id }}" {{ $user->hasRole($role->name)?'checked':'' }}>
-            <label for="checkbox{{ $role->id }}">{{ $role->name }}</label>
-        </fieldset>
-    @endforeach
-    <input type="submit" class="btn btn-primary" value="@lang('Submit')">
-</form>
+<div class="page-name-xl">@lang('User Roles')</div>
+<div class="row">
+    <div class="col-md-6">
+    </div>
+</div>
+<div class="row">
+    <div class="col col-md-5 mx-auto">
+        <form method="post" action="{{ url('user/roles/' . $user->id) }}">
+            {{ csrf_field() }}
+            @foreach($roles as $role)
+                <div class="switch switch_sty d-flex">
+                    <span for="checkbox{{ $role->id }}" class="switch_label mr-auto">{{ $role->name }}</span>
+                    <label class="">Off
+                        <input type="checkbox" id="checkbox{{ $role->id }}" name="roles[]" value="{{ $role->id }}" {{ $user->hasRole($role->name)?'checked':'' }}>
+                        <span class="lever"></span>
+                      On
+                    </label>
+                </div>
+            @endforeach
+        </form>
+    </div>
+</div>
+<!-- Footer buttons --> 
+<div class="btn-footer d-flex p-2">
+    <button type="submit" class="btn btn-lg btn-save">@lang('Submit')</button>
+    <button type="submit" class="btn btn-lg btn-cancel">@lang('Cancel')</button>
+</div>
+<!--/. Footer buttons -->
 @endsection
