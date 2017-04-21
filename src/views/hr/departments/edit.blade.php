ca8ada2ccdced0397aa9da803db23a07387be7e8
StjepanD <sdrmic@gmail.com>
2017-04-21 10:22:28 +0200

    New department styling

--- src/views/hr/departments/edit.blade.php
+++ src/views/hr/departments/edit.blade.php
@@ -2,12 +2,27 @@
 
 @section('content')
 
-    <form action="{{ url('hr/department/edit/'.$department->id) }}" method="post">
-        {{ csrf_field() }}
-        <div class="form-group">
-            <label for="name">@lang("Name")</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="@lang("Enter Name")" required value="{{ $department->name }}">
+
+    <div class="page-name-xl">@lang('New department')</div>
+    <div class="row">
+        <div class="col-md-6">
         </div>
-        <button type="submit" class="btn btn-primary">@lang('Submit')</button>
-    </form>
+    </div>
+    <div class="row">
+        <div class="col col-md-5 mx-auto">      
+            <form action="{{ url('hr/department/edit/'.$department->id) }}" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="name">@lang("Name")</label>
+                    <input type="text" class="form-control" id="name" name="name" placeholder="@lang("Enter Name")" required value="{{ $department->name }}">
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- Footer buttons --> 
+    <div class="btn-footer d-flex p-2">
+        <button type="submit" class="btn btn-lg btn-save">@lang('Submit')</button>
+        <button type="submit" class="btn btn-lg btn-cancel">@lang('Cancel')</button>
+    </div>
+    <!--/. Footer buttons -->
 @endsection
