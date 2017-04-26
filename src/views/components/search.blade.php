de3d46e9a7eeb8c3c931fda45baac3ab0ef21007
StjepanD <sdrmic@gmail.com>
2017-04-26 16:36:28 +0200

    Table sort icons, search icon remove

--- src/views/components/search.blade.php
+++ src/views/components/search.blade.php
@@ -2,7 +2,7 @@
 <div class="md-form input-group search-big">
     <input type="search" class="form-control" placeholder="@lang('Search...')">
     <span class="input-group-btn">
-            <a class="btn search-big-button" type="button"><i class="fa fa-search fa-4x left"></i></a>
+            <a class="btn search-big-button" type="button"></a>
         </span>
 </div>
 <!--/. Search -->
