ddf1bf4278df3ea4c3d63ca4e3fdb882f9a1a9a4
StjepanD <sdrmic@gmail.com>
2017-10-16 14:56:29 +0200

    Approve higer cost

--- src/assets/js/components/proposition/expenses/Compare.vue
+++ src/assets/js/components/proposition/expenses/Compare.vue
@@ -227,7 +227,7 @@
             <tbody class="white">
             <tr>
                 <th scope="row">1</th>
-                <td data-title="@lang('Title')">Layout and Design budget</td>
+                <td data-title="@lang('Title')">{{ lang('Layout and Design budget') }}</td>
                 <td data-title="@lang('Budget')">{{ production_expense.budget.totals.layout | flexCurrency(' kn', 2) }}</td>
                 <td data-title="@lang('Expense Total')">{{ production_expense.expense.totals.layout | flexCurrency(' kn', 2) }}</td>
                 <td data-title="@lang('Difference')">{{ Math.abs(diff = production_expense.budget.totals.layout - production_expense.expense.totals.layout) | flexCurrency(' kn', 2) }}</td>
@@ -264,7 +264,156 @@
             <button class="btn btn-lg btn-save" v-on:click="saveExpenses">{{ lang('Save') }}</button>
 
         </div>
+        
+        <!-- Central Modal Medium Assign Tab -->
+        <div class="modal fade" id="ModalCostApprove" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-notify modal-warning" role="document">
+                <!--Content-->
+                <div class="modal-content">
+                    <!--Header-->
+                    <div class="modal-header flex-column px-3 pt-3">
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true" class="white-text">&times;</span>
+                        </button>
+                        <div class="d-flex">
+                            <i class="fa fa-user-circle-o fa-4x mb-1 animated rotateInDownLeft"></i>
+                            <h1 class="modal-title w-100 text-center">{{ lang('Assign to...') }}</h1>
+                        </div>
+                        <h6 class="w-100 text-center mb-2">{{ lang('Assign department or directly employee') }}</h6>
+                    </div>
 
+                    <!-- Nav tabs -->
+                    <div class="tabs-wrapper">
+                        <ul class="nav classic-tabs tabs-cyan tab-full" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link waves-light active" data-toggle="tab" href="#panel51" role="tab">{{ lang('Department') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link waves-light" data-toggle="tab" href="#panel52" role="tab">{{ lang('Employee') }}</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Tab panels -->
+                    <div class="tab-content">
+
+                        <!--Panel 1-->
+                        <div class="modal-body tab-pane fade in show active" id="panel51" role="tabpanel">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="md-form d-flex addon">
+                                        <input type="text" class="form-control" name="department" placeholder="Pronađi odjel" v-model="department" v-on:keyup="autocomplete($event, 'department')">
+                                        <ul class="mdb-autocomplete-wrap" v-if="d_suggestions.length">
+                                            <li v-for="(item, index) in d_suggestions" v-on:click="autocomplete_select(index, 'department')">{{ item.name }}</li>
+                                        </ul>
+                                    </div>
+                                    <div class="chip mb-5" v-for="department in departments">
+                                        {{ department.name }}<i class="close fa fa-times"></i>
+                                    </div>
+                                    <!-- Textarea -->
+                                    <div class="md-form mt-2 mb-2">
+                                        <textarea id="description" class="md-textarea" v-model="description"></textarea>
+                                        <label for="description">{{ lang('Task Description') }}</label>
+                                    </div>
+                                    <!-- Expense difference -->
+                                    <table class="table">
+                                        <thead class="thead-inverse">
+                                        <tr>
+                                            <th>{{ lang('Item') }}</th>
+                                            <th>{{ lang('Budget') }}</th>
+                                            <th>{{ lang('Expense Total') }}</th>
+                                            <th>{{ lang('Difference') }}</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody class="white">
+                                        <tr>
+                                            <td data-title="@lang('Title')">Marketing budget</td>
+                                            <td data-title="@lang('Budget')">{{ marketing_expense.budget?marketing_expense.budget.totals:0 | flexCurrency(' kn', 2) }}</td>
+                                            <td data-title="@lang('Expense Total')">{{ marketing_expense.expense?marketing_expense.expense.totals:0 | flexCurrency(' kn', 2) }}</td>
+                                            <td data-title="@lang('Difference')">{{ Math.abs(diff = marketing_expense.budget.totals - marketing_expense.expense.totals) | flexCurrency(' kn', 2) }}</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <!--/. Expense difference -->
+                                </div>
+                            </div>
+                        </div>
+                        <!--/.Panel 1-->
+
+                        <!--Panel 2-->
+                        <div class="modal-body tab-pane fade" id="panel52" role="tabpanel">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="md-form d-flex addon">
+                                        <input type="text" class="form-control" name="employee" placeholder="Pronađi osobu" v-model="employee" v-on:keyup="autocomplete($event, 'employee')">
+                                        <ul class="mdb-autocomplete-wrap" v-if="e_suggestions.length">
+                                            <li v-for="(item, index) in e_suggestions" v-on:click="autocomplete_select(index, 'employee')">{{ item.name }}</li>
+                                        </ul>
+                                    </div>
+                                    <div class="chip mb-5" v-for="employee in employees">
+                                        <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-6.jpg">{{ employee.name }}<i class="close fa fa-times"></i>
+                                    </div>
+                                    <!-- Textarea -->
+                                    <div class="md-form mt-2 mb-2">
+                                        <textarea id="form76" class="md-textarea" v-model="description"></textarea>
+                                        <label for="form76">{{ lang('Task Description') }}</label>
+                                    </div>
+                                    <!-- Date Picker -->
+                                    <div class="row mt-4">
+                                        <div class="col-md-5">
+                                            <div class="md-form">
+                                                <input placeholder="Selected date" type="text" id="date-picker2" class="form-control datepicker btn-white" v-model="date">
+                                                <label for="date-picker2">{{ lang('Select Date') }}</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!--/. Checkbox -->
+                                    <div class="page-name-m">{{ lang('Priority') }}</div>
+                                    <div class="form-inline mb-3">
+                                        <fieldset class="form-group">
+                                            <input name="priority1" type="radio" id="radio61" value="high" v-model="priority" >
+                                            <label for="radio61">{{ lang('High') }}</label>
+                                        </fieldset>
+                                        <fieldset class="form-group">
+                                            <input name="priority1" type="radio" id="radio71" value="medium" v-model="priority">
+                                            <label for="radio71">{{ lang('Medium') }}</label>
+                                        </fieldset>
+                                        <fieldset class="form-group">
+                                            <input name="priority1" type="radio" id="radio81" value="low" v-model="priority">
+                                            <label for="radio81">{{ lang('Low') }}</label>
+                                        </fieldset>
+                                    </div>
+                                    <!--/. Checkbox -->
+                                    <!-- Checkbox -->
+                                    <div class="page-name-m">{{ lang('Access Level') }}</div>
+                                    <div class="form-inline mb-3">
+                                        <fieldset class="form-group">
+                                            <input checked name="access1" type="radio" id="radio91" value="allpage" v-model="access">
+                                            <label for="radio91">{{ lang('All Proposition Pages') }}</label>
+                                        </fieldset>
+                                        <fieldset class="form-group">
+                                            <input name="access1" type="radio" id="radio101" value="onepage" v-model="access">
+                                            <label for="radio101">{{ lang('Only This Page') }}</label>
+                                        </fieldset>
+                                    </div>
+                                    <!--/. Checkbox -->
+                                </div>
+                            </div>
+                        </div>
+                        <!--/.Panel 2-->
+                    </div>
+
+                    <!--Footer-->
+                    <div class="modal-footer btn-footer">
+                        <button type="button" class="btn btn-lg btn-cancel" data-dismiss="modal">{{ lang('Cancel') }}</button>
+                        <button type="button" class="btn btn-lg btn-save" data-dismiss="modal" v-on:click="assignValues">{{ lang('Assign') }}</button>
+                    </div>
+                </div>
+                <!--/.Content-->
+            </div>
+        </div>
+        <!-- Central Modal Medium Assign tab-->
+        
     </div>
 
 </template>
