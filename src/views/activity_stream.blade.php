1847d7f09588cd3cea1075fc3b65843a7256bda6
Marko Banusic <mbanusic@gmail.com>
2017-12-14 09:48:26 +0100

    Notification blade

--- src/views/activity_stream.blade.php
+++ src/views/activity_stream.blade.php
@@ -7,101 +7,29 @@
     <div class="content">
         <div class="page-name-xl mb-1 mt-2">@lang('Activity Stream')</div>
         <h4 class="activity-day weight-700 text-uppercase">@lang('Today')</h4>
-        
+        @foreach ($notifications as $notification)
         <div class="activity align-items-center mt-1">
-            <div class="activity-label mx-1 tasktype-1">Nešto
-            </div>
+            @if(isset($notification->data['tasktype']))
+                <div class="activity-label mx-1 tasktype-1">{{ $notification->data['tasktype'] }}</div>
+            @endif
             <div class="activity-avatar file-box-sty d-flex align-items-center">
-                <a href="">
-                    <img class="profile-l mx-3" src="../images/profile.jpg" href="#">
+                <a href="{{ $notification->data['sender']['link'] }}">
+                    <img class="profile-l mx-3" src="{{ $notification->data['sender']['image'] }}">
                 </a>
             </div>
             <div class="activity-content">
-                <h4 class="activitiy-user"><span>Jozo Jozić</span> poslao(la) je novu<span><a href=""> poruku</a></span></h4>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi fugit vel quisquam.</h5>
-                <div class="activity-time"><span>4 </span>@lang('Hours')</div>
+                <h4 class="activitiy-user"><a href="{{ $notification->data['link'] }}">{{ $notification->data['title'] }}</a></h4>
+                <h5>{{ $notification->data['message'] }}</h5>
+                <div class="activity-time"></div>
             </div>
         </div>
-        <div class="activity align-items-center">
-            <div class="activity-label mx-1 tasktype-1">Nešto
-            </div>
-            <div class="activity-avatar file-box-sty d-flex align-items-center">
-                <a href="">
-                    <img class="profile-l mx-3" src="../images/profile.jpg" href="#">
-                </a>
-            </div>
-            <div class="activity-content">
-                <h4 class="activitiy-user"><span>Jozo Jozić</span> poslao(la) je novu<span><a href=""> poruku</a></span></h4>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi fugit vel quisquam.</h5>
-                <div class="activity-time"><span>4 </span>@lang('Hours')</div>
-            </div>
-        </div>
-        <div class="activity align-items-center">
-            <div class="activity-label mx-1 tasktype-1">Nešto
-            </div>
-            <div class="activity-avatar file-box-sty d-flex align-items-center">
-                <a href="">
-                    <img class="profile-l mx-3" src="../images/profile.jpg" href="#">
-                </a>
-            </div>
-            <div class="activity-content">
-                <h4 class="activitiy-user"><span>Jozo Jozić</span> poslao(la) je novu<span><a href=""> poruku</a></span></h4>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi fugit vel quisquam.</h5>
-                <div class="activity-time"><span>4 </span>@lang('Hours')</div>
-            </div>
-        </div>
-        <div class="activity align-items-center">
-            <div class="activity-label mx-1 tasktype-1">Nešto
-            </div>
-            <div class="activity-avatar file-box-sty d-flex align-items-center">
-                <a href="">
-                    <img class="profile-l mx-3" src="../images/profile.jpg" href="#">
-                </a>
-            </div>
-            <div class="activity-content">
-                <h4 class="activitiy-user"><span>Jozo Jozić</span> poslao(la) je novu<span><a href=""> poruku</a></span></h4>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi fugit vel quisquam.</h5>
-                <div class="activity-time"><span>4 </span>@lang('Hours')</div>
-            </div>
-        </div>
-        
-        
+        @endforeach
+
+
         <h4 class="activity-day weight-700 text-uppercase mt-5">@lang('Older Activity')</h4>
-        
-        <div class="activity align-items-center mt-1">
-            <div class="activity-label mx-1 tasktype-1">Nešto
-            </div>
-            <div class="activity-avatar file-box-sty d-flex align-items-center">
-                <a href="">
-                    <img class="profile-l mx-3" src="../images/profile.jpg" href="#">
-                </a>
-            </div>
-            <div class="activity-content">
-                <h4 class="activitiy-user"><span>Jozo Jozić</span> poslao(la) je novu<span><a href=""> poruku</a></span></h4>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi fugit vel quisquam.</h5>
-                <div class="activity-time"><span>4 </span>@lang('Hours')</div>
-            </div>
-        </div>
-        <div class="activity align-items-center">
-            <div class="activity-label mx-1 tasktype-1">Nešto
-            </div>
-            <div class="activity-avatar file-box-sty d-flex align-items-center">
-                <a href="">
-                    <img class="profile-l mx-3" src="../images/profile.jpg" href="#">
-                </a>
-            </div>
-            <div class="activity-content">
-                <h4 class="activitiy-user"><span>Jozo Jozić</span> poslao(la) je novu<span><a href=""> poruku</a></span></h4>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi fugit vel quisquam.</h5>
-                <div class="activity-time"><span>4 </span>@lang('Hours')</div>
-            </div>
-        </div>
 
-        
+
     </div>
-    
-
-
 @endsection
 
 
