1d1996e0042a73c84a91df8bad79d3f00c3ea861
Marko Banusic <mbanusic@gmail.com>
2017-12-15 12:09:51 +0100

    Move flipclock translation to separate file

--- src/assets/js/bootstrap.js
+++ src/assets/js/bootstrap.js
@@ -29,31 +29,3 @@ window.Echo = new Echo({
     key: '2fe97cc7ad11d260f148',
     cluster: 'eu'
 });
-
-(function($) {
-
-    /**
-     * FlipClock English Language Pack
-     *
-     * This class will used to translate tokens into the English language.
-     *
-     */
-
-    FlipClock.Lang.Croatian = {
-
-        'years'   : 'Godine',
-        'months'  : 'Mjeseci',
-        'days'    : 'Dani',
-        'hours'   : 'Sati',
-        'minutes' : 'Minute',
-        'seconds' : 'Sekunde'
-
-    };
-
-    /* Create various aliases for convenience */
-
-    FlipClock.Lang['hr']      = FlipClock.Lang.Croatian;
-    FlipClock.Lang['hr-hr']   = FlipClock.Lang.Croatian;
-    FlipClock.Lang['croatian'] = FlipClock.Lang.Croatian;
-
-}(jQuery));
---
+++ src/assets/js/fixes/hr-hr.js
@@ -0,0 +1,27 @@
+(function($) {
+
+    /**
+     * FlipClock English Language Pack
+     *
+     * This class will used to translate tokens into the English language.
+     *
+     */
+
+    FlipClock.Lang.Croatian = {
+
+        'years'   : 'Godine',
+        'months'  : 'Mjeseci',
+        'days'    : 'Dani',
+        'hours'   : 'Sati',
+        'minutes' : 'Minute',
+        'seconds' : 'Sekunde'
+
+    };
+
+    /* Create various aliases for convenience */
+
+    FlipClock.Lang['hr']      = FlipClock.Lang.Croatian;
+    FlipClock.Lang['hr-hr']   = FlipClock.Lang.Croatian;
+    FlipClock.Lang['croatian'] = FlipClock.Lang.Croatian;
+
+}(jQuery));
