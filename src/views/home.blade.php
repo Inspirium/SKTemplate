783c259924f7b8d005b21899e7366af9c168c1a0
Marko Banusic <mbanusic@gmail.com>
2018-01-04 16:21:18 +0100

    Editions on homepage

--- src/views/home.blade.php
+++ src/views/home.blade.php
@@ -38,180 +38,36 @@
    <div class="chart mt-4">
        <div class="header">
            <div class="header__title--style">Pregled projekata</div>
-<!--           <div class="header__desc--style">ovo je opis grafa ako je potreban. Ako nije, nećemo ga prikazati</div>-->
        </div>
-<!--
-       <div class="filter">
-           <div class="filter-chip--style">Tjedan</div>
-           <div class="filter-chip--style">Mjesec</div>
-           <div class="filter-chip--style active">Godina</div>
-       </div>
--->
+
        <div class="content mt-3">
            <div class="ct-chart ct-double-octave" id="chart1"></div>
        </div>
    </div>
-<!--
-   <div class="chart mt-4">
-       <div class="header">
-           <div class="header__title--style">Naziv grafa</div>
-           <div class="header__desc--style">ovo je opis grafa ako je potreban. Ako nije, nećemo ga prikazati</div>
-       </div>
-       <div class="filter">
-           <div class="filter-chip--style">Tjedan</div>
-           <div class="filter-chip--style">Mjesec</div>
-           <div class="filter-chip--style active">Godina</div>
-       </div>
-       <div class="content mt-3">
-           <div class="ct-chart ct-double-octave" id="chart2"></div>
-       </div>
-   </div>
-
-    <div class="row">
-        <div class="col-md-6">
-           <div class="chart mt-4">
-               <div class="header">
-                   <div class="header__title--style">Naziv grafa</div>
-                   <div class="header__desc--style">ovo je opis grafa ako je potreban. Ako nije, nećemo ga prikazati</div>
-               </div>
-               <div class="filter">
-                   <div class="filter-chip--style">Tjedan</div>
-                   <div class="filter-chip--style">Mjesec</div>
-                   <div class="filter-chip--style active">Godina</div>
-               </div>
-               <div class="content mt-3">
-                   <div class="ct-chart ct-double-octave" id="chart3"></div>
-               </div>
-           </div>
-        </div>
-        <div class="col-md-6">
-           <div class="chart mt-4">
-               <div class="header">
-                   <div class="header__title--style">Naziv grafa</div>
-                   <div class="header__desc--style">ovo je opis grafa ako je potreban. Ako nije, nećemo ga prikazati</div>
-               </div>
-               <div class="filter">
-                   <div class="filter-chip--style">Tjedan</div>
-                   <div class="filter-chip--style">Mjesec</div>
-                   <div class="filter-chip--style active">Godina</div>
-               </div>
-               <div class="content mt-3">
-                   <div class="ct-chart ct-double-octave" id="chart4"></div>
-               </div>
-           </div>
-        </div>
-    </div>
-
--->
-
-
         <div class="row">
             <div class="col-md-12">
                 <div class="page-name-xl mb-1 mt-3">@lang('New Editions')</div>
                 <div class="card-box">
                     <div class="row">
+                        @foreach ($editions as $edition)
                         <!--/.Card-->
                         <div class="col-md-3 align-self-end">
                             <!--Card-->
-                            <a class="card mb-4">
+                            <a class="card mb-4" href="{{ $edition->link }}">
                                 <!--Card image-->
-                                <img class="img-fluid" src="images/061809.jpg" alt="Card image cap">
+                                <img class="img-fluid" src="{{ $edition->cover }}" alt="Card image cap">
 
                                 <!--Card content-->
                                 <div class="card-body">
                                     <!--Title-->
-                                    <h4 class="card-title">Alfabetist</h4>
+                                    <h4 class="card-title">{{ $edition->title }}</h4>
                                     <!--Text-->
-                                    <div class="card-text"><span class="font-weight-bold">Autor: </span>Torsten Petterson</div>
-                                    <div class="card-text"><span class="font-weight-bold">Završeno: </span>21. 11. 2017.</div>
+                                    <div class="card-text"><span class="font-weight-bold">Autor: </span>{{ $edition->formattedAuthors }}</div>
+                                    <div class="card-text"><span class="font-weight-bold">Završeno: </span>{{ $edition->created_at }}</div>
                                 </div>
                             </a>
                         </div>
-                        <!--/.Card-->
-                        <div class="col-md-3 align-self-end">
-                            <!--Card-->
-                            <a class="card mb-4">
-                                <!--Card image-->
-                                <img class="img-fluid" src="images/061756.jpg" alt="Card image cap">
-
-                                <!--Card content-->
-                                <div class="card-body">
-                                    <!--Title-->
-                                    <h4 class="card-title">Alfabetist</h4>
-                                    <!--Text-->
-                                    <div class="card-text"><span class="font-weight-bold">Autor: </span>Torsten Petterson</div>
-                                    <div class="card-text"><span class="font-weight-bold">Završeno: </span>21. 11. 2017.</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!--/.Card-->
-                        <div class="col-md-3 align-self-end">
-                            <!--Card-->
-                            <a class="card mb-4">
-                                <!--Card image-->
-                                <img class="img-fluid" src="images/061735.jpg" alt="Card image cap">
-
-                                <!--Card content-->
-                                <div class="card-body">
-                                    <!--Title-->
-                                    <h4 class="card-title">Alfabetist</h4>
-                                    <!--Text-->
-                                    <div class="card-text"><span class="font-weight-bold">Autor: </span>Torsten Petterson</div>
-                                    <div class="card-text"><span class="font-weight-bold">Završeno: </span>21. 11. 2017.</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!--/.Card-->
-                        <div class="col-md-3 align-self-end">
-                            <!--Card-->
-                            <a class="card mb-4">
-                                <!--Card image-->
-                                <img class="img-fluid" src="images/061809.jpg" alt="Card image cap">
-
-                                <!--Card content-->
-                                <div class="card-body">
-                                    <!--Title-->
-                                    <h4 class="card-title">Alfabetist</h4>
-                                    <!--Text-->
-                                    <div class="card-text"><span class="font-weight-bold">Autor: </span>Torsten Petterson</div>
-                                    <div class="card-text"><span class="font-weight-bold">Završeno: </span>21. 11. 2017.</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!--/.Card-->
-                        <div class="col-md-3 align-self-end">
-                            <!--Card-->
-                            <a class="card mb-4">
-                                <!--Card image-->
-                                <img class="img-fluid" src="images/061756.jpg" alt="Card image cap">
-
-                                <!--Card content-->
-                                <div class="card-body">
-                                    <!--Title-->
-                                    <h4 class="card-title">Alfabetist</h4>
-                                    <!--Text-->
-                                    <div class="card-text"><span class="font-weight-bold">Autor: </span>Torsten Petterson</div>
-                                    <div class="card-text"><span class="font-weight-bold">Završeno: </span>21. 11. 2017.</div>
-                                </div>
-                            </a>
-                        </div>
-                        <!--/.Card-->
-                        <div class="col-md-3 align-self-end">
-                            <!--Card-->
-                            <a class="card mb-4">
-                                <!--Card image-->
-                                <img class="img-fluid" src="images/061735.jpg" alt="Card image cap">
-
-                                <!--Card content-->
-                                <div class="card-body">
-                                    <!--Title-->
-                                    <h4 class="card-title">Alfabetist</h4>
-                                    <!--Text-->
-                                    <div class="card-text"><span class="font-weight-bold">Autor: </span>Torsten Petterson</div>
-                                    <div class="card-text"><span class="font-weight-bold">Završeno: </span>21. 11. 2017.</div>
-                                </div>
-                            </a>
-                        </div>
+                        @endforeach
 
 
 
