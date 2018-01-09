ea2fb56b18b4022fdc28837174f87533a4b7631b
StjepanD <sdrmic@gmail.com>
2018-01-09 15:01:49 +0100

    Reset page style

--- src/views/auth/passwords/reset.blade.php
+++ src/views/auth/passwords/reset.blade.php
@@ -1,13 +1,18 @@
 @extends('sktemplate::layout')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+<div class="login">
+    <div class="container">
 
-                <div class="panel-body">
+        <div class="col card mx-auto">
+            <div class="card-block">
+                    <!--Header-->
+                    <div class="form-header form-header-reset">
+                        <h2>Resetiraj lozinku</h2>
+                    </div>
+
+                    <!--Body-->
+                    <div class="panel-body">
                     @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
@@ -20,9 +25,9 @@
                         <input type="hidden" name="token" value="{{ $token }}">
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="control-label">E-mail adresa</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -34,9 +39,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="control-label">Lozinka</label>
 
-                            <div class="col-md-6">
+                            <div class="">
                                 <input id="password" type="password" class="form-control" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -48,8 +53,8 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
+                            <label for="password-confirm" class="control-label">Potvrdi lozinku</label>
+                            <div class="">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
                                 @if ($errors->has('password_confirmation'))
@@ -61,9 +66,9 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Reset Password
+                            <div class="">
+                                <button type="submit" class="btn btn-success">
+                                    Resetiraj lozinku
                                 </button>
                             </div>
                         </div>
@@ -71,6 +76,7 @@
                 </div>
             </div>
         </div>
+
     </div>
 </div>
 @endsection
