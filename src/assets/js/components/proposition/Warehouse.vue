4dfc9e83727d21d3615efe6ace00be5cd5495939
Stjepan Drmic <sdrmic@gmail.com>
2018-03-12 10:35:48 +0100

    Warehous styling

--- src/assets/js/components/proposition/Warehouse.vue
+++ src/assets/js/components/proposition/Warehouse.vue
@@ -1,12 +1,18 @@
 <template>
     <div class="content">
-        <div class="page-name-xl mb-4 mt-3">{{ lang('Završni dokument') }}</div>
+        <div class="page-name-xl mb-4 mt-3">{{ lang('Završetak projekta') }}</div>
         <div class="justify-content-center d-flex mb-4" v-if="validated">
             <spinner-button v-on:button_clicked="sendToWarehouse" v-on:button_cleanup_success="redirect" v-bind:title="'Recieved in Warehouse'" v-bind:classes="'btn btn-l btn-neutral'"></spinner-button>
         </div>
         <div class="justify-content-center d-flex mb-4" v-else>
             <ul>
-                <li v-for="field in fields"><router-link v-bind:to="field.link">{{ lang(field.title )}}</router-link></li>
+                <div class="d-flex justify-content-center align-items-center flex-column mt-2">
+                    <i class="fa fa-exclamation-triangle fa-5x mb-3 color-nav-sub" aria-hidden="true"></i>
+                    <h1 class="text-center mt-2">{{ lang('Da biste uspješno završili projekt, potrebni su sljedeći podaci:') }}</h1>
+                </div>
+                <li class="warehouse-list mt-3" v-for="field in fields">
+                    <router-link v-bind:to="field.link">{{ lang(field.title )}}</router-link>
+                </li>
             </ul>
         </div>
     </div>
